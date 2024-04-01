31d14d9eaef86c39f787a5c472f2f5b4dfe4d7a8
j1sung <dalssagi00@gmail.com>
2024-04-02 01:36:28 +0900

    ✨ BOJ_C++

--- BOJ/C++/1978.cpp
+++ BOJ/C++/1978.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 int main(){
-    int n=0, cnt=0;
-    scanf(%d, &n);
-    int arr[1000];
+    int n=0, cnt=0, res=0;
+    scanf("%d", &n);
+
     for(int i=0; i<n; ++i)
     {
-        scanf(%d, &arr[i]);
-        for(int j=2; j<arr[i]; ++j)
+        int val=0;
+        scanf("%d", &val);
+        for(int j=2; j<val; ++j)
         {
-            if(arr[i]%j==0)
-                break;
-                
+            if(val%j==0)
+                ++cnt;
         }
+        if(cnt==0)
+            ++res;
+        cnt=0;
     }
     
-
+    printf("%d", res);
 
     return 0;
 }
