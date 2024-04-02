2554774a62dd1a02a2f363548d221d60f117dbea
j1sung <dalssagi00@gmail.com>
2024-04-02 17:56:29 +0900

    ✨ BOJ_C++

--- BOJ/C++/1978.cpp
+++ BOJ/C++/1978.cpp
@@ -1,22 +1,25 @@
-#include<cstdio>
+#include <cstdio>
 using namespace std;
 
 int main(){
     int n=0, cnt=0, res=0;
-    scanf("%d", &n);
+    scanf("%d", &n); // 1<=N<=100
 
     for(int i=0; i<n; ++i)
     {
-        int val=0;
-        scanf("%d", &val);
-        for(int j=2; j<val; ++j)
+        int val;
+        scanf("%d", &val); //1<=val<=1000
+        if(val!=1)
         {
-            if(val%j==0)
-                ++cnt;
+            for(int j=2; j<val; ++j)
+            {
+                if(val%j==0)
+                    ++cnt;
+            }
+            if(cnt==0)
+                ++res;
+            cnt=0;
         }
-        if(cnt==0)
-            ++res;
-        cnt=0;
     }
     
     printf("%d", res);
